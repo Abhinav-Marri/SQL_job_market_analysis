34a1726da9af474426de9e55b7477049622fcdbb
Abhinav Marri <111858442+Abhinav-Marri@users.noreply.github.com>
2025-12-16 00:20:01 +0530

    Add files via upload

---
+++ sql_queries/skills.sql
@@ -0,0 +1,25 @@
+WITH top_jobs AS
+(
+    SELECT
+    job_id,
+    job_title_short,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Cloud Engineer' AND salary_year_avg IS NOT NULL AND job_location = 'Anywhere'
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+)
+SELECT 
+    top_jobs.*,
+    skills
+FROM
+   top_jobs
+INNER JOIN skills_job_dim ON top_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
---
+++ sql_queries/top_paying_jobs.sql
@@ -0,0 +1,13 @@
+SELECT
+    job_title_short,
+    job_location,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Cloud Engineer' AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
